371f4525a9ac9581b563d22f4263675600d6a8ed
Guest User <Guest@Epicodus-1C.local>
2017-10-12 13:14:13 -0700

    make better.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Rename.Models;
 
 namespace Rename.Controllers
 {
     public class HomeController : Controller
     {
-      
+
     }
 }
---
+++ Models/Variable.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Rename.Models;
+{
+  public class Variable
+  {
+
+  }
+}
